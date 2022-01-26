a590283c2516b96a6504ed73f717e7c626090238
Issifou Issa <chrisissifou@gmail.com>
2022-01-26 09:29:20 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const tab = [
+    { id:1, lastanme: "Issifou", firstname: "Issa" },
+    { id:2, lastanme: "Goya", firstname: "Mame" },
+    { id:3, lastanme: "Mauriade", firstname: "Paul" },
+    { id:4, lastanme: "Conard", firstname: "Giles" }
+]
+
+console.log(tab.find(element => element.id === 2))
